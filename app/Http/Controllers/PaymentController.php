1a3658f492f420e12c20c930d26d10e5f40e2c3c
Vikas-tiwari-developer <77283008+Vikas-tiwari-developer@users.noreply.github.com>
2023-08-07 22:43:10 +0530

    Update PaymentController.php

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -41,4 +41,110 @@ class PaymentController extends Controller
         \Session::put('success', 'Payment successful');
         return redirect()->back();
     }
+
+    // PhonePe Integration
+    public function makePhonePePayment(Request $request)
+    {
+        $amount = session()->get('phonepe_amount') ?? 100;
+        $data = array (
+          'merchantId' => 'MERCHANTUAT',
+          'merchantTransactionId' => 'MT7850590068188103',
+          'merchantUserId' => 'MUID123',
+          'amount' => $amount * 100,
+          'redirectUrl' => route('phonepe.payment.callback'),
+          'redirectMode' => 'POST',
+          'callbackUrl' => route('phonepe.payment.callback'),
+          'mobileNumber' => '9999999999',
+          'paymentInstrument' => 
+          array (
+            'type' => 'PAY_PAGE',
+          ),
+        );
+
+        $encode = base64_encode(json_encode($data));
+
+        $saltKey = '099eb0cd-02cf-4e2a-8aca-3e6c6aff0399';
+        $saltIndex = 1;
+
+        $string = $encode.'/pg/v1/pay'.$saltKey;
+        $sha256 = hash('sha256',$string);
+
+        $finalXHeader = $sha256.'###'.$saltIndex;
+
+        // $response = Curl::to('https://api-preprod.phonepe.com/apis/merchant-simulator/pg/v1/pay')
+        //         ->withHeader('Content-Type:application/json')
+        //         ->withHeader('X-VERIFY:'.$finalXHeader)
+        //         ->withData(json_encode(['request' => $encode]))
+        //         ->post();
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => 'https://api-preprod.phonepe.com/apis/merchant-simulator/pg/v1/pay',
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => false,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'POST',
+          CURLOPT_POSTFIELDS => json_encode(['request' => $encode]),
+          CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json',
+            'X-VERIFY: '.$finalXHeader
+          ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        $rData = json_decode($response);
+
+        return redirect()->to($rData->data->instrumentResponse->redirectInfo->url);
+
+    }
+
+    public function phonePeCallback(Request $request)
+    {
+        $input = $request->all();
+
+        $saltKey = '099eb0cd-02cf-4e2a-8aca-3e6c6aff0399';
+        $saltIndex = 1;
+
+        $finalXHeader = hash('sha256','/pg/v1/status/'.$input['merchantId'].'/'.$input['transactionId'].$saltKey).'###'.$saltIndex;
+
+        // $response = Curl::to('https://api-preprod.phonepe.com/apis/merchant-simulator/pg/v1/status/'.$input['merchantId'].'/'.$input['transactionId'])
+        //         ->withHeader('Content-Type:application/json')
+        //         ->withHeader('accept:application/json')
+        //         ->withHeader('X-VERIFY:'.$finalXHeader)
+        //         ->withHeader('X-MERCHANT-ID:'.$input['transactionId'])
+        //         ->get();
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => 'https://api-preprod.phonepe.com/apis/merchant-simulator/pg/v1/status/'.$input['merchantId'].'/'.$input['transactionId'],
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => false,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'GET',
+          CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json',
+            'accept: application/json',
+            'X-VERIFY: '.$finalXHeader,
+            'X-MERCHANT-ID: '.$input['transactionId']
+          ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        dd(json_decode($response));
+        // flash(translate('Your order has been placed successfully. Please submit payment information from purchase history'))->success();
+        // return redirect()->route('order_confirmed');
+    }
 }
